91c987ecda222d53b1486bd002ac46ba2d981b3c
taeeun kim <kimtaeeeny@gmail.com>
2020-08-29 02:28:00 +0900

    quantize module test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
+const arrayOfPixels = [
+  [255, 0, 0],
+  [0, 255, 0],
+  [0, 0, 255],
+  [255, 255, 0],
+  [0, 255, 255],
+]
+
+const quantize = require("quantize")
+
+const maximumColorCount = 5
+
+const colorMap = quantize(arrayOfPixels, maximumColorCount)
+const quantizedColors = colorMap.palette()
+console.log(quantizedColors)
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        {arrayOfPixels.map((item, i) => {
+          return (
+            <div
+              style={{
+                backgroundColor: `rgb(${arrayOfPixels[i][0]},${arrayOfPixels[i][1]},${arrayOfPixels[i][2]}`,
+                width: `${100 / arrayOfPixels.length}%`,
+                height: `100vh`,
+              }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className="App">
+        {quantizedColors.map((item, i) => {
+          return (
+            <div
+              style={{
+                backgroundColor: `rgb(${quantizedColors[i][0]},${quantizedColors[i][1]},${quantizedColors[i][2]}`,
+                width: `${100 / quantizedColors.length}%`,
+                height: `100vh`,
+              }}
+            ></div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
